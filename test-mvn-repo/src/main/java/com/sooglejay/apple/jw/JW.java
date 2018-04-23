44ce0afcad47c08be563776e6c330b9329f96406
sooglejay <394055524@qq.com>
2018-04-23 10:48:51 +0800

    add all files

---
+++ test-mvn-repo/src/main/java/com/sooglejay/apple/jw/JW.java
@@ -0,0 +1,7 @@
+package com.sooglejay.apple.jw;
+//import com.sooglejay.book.helloworld.helloworld.HelloWorld;
+public class JW{
+   public static void main(String args[]){
+       System.out.println("jiangwei learns maven!:");
+   }
+}
---
+++ test-mvn-repo/src/test/java/JWTest.java
@@ -0,0 +1,13 @@
+package com.sooglejay.book.jw;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class JWTest{
+   
+   @Test
+   public void testSayHello(){
+     
+
+     
+   }
+}
